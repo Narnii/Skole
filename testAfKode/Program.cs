0a3059eb74d433ecfa5eeb789f98f9c799d2ffeb
Kristina <Kristina@DESKTOP-K92JREV>
2017-09-14 10:37:29 +0200

    Add project files.

---
+++ testAfKode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testAfKode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] perts =  { "Hund", "Kat" };
+            foreach(string val in perts)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
